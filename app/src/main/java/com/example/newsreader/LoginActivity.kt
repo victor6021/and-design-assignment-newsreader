5cbc4b9b31c19bf4a8d68b5f21eeb9e3a19803ee
victor6021 <victor60216021@gmail.com>
2019-08-14 15:21:25 -0700

    victor ha-news reader
    updated: added listeners to all images.

---
+++ app/src/main/java/com/example/newsreader/LoginActivity.kt
@@ -0,0 +1,24 @@
+package com.example.newsreader
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+    companion object {
+        const val REQUEST_CODE = 2
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        sign_in.setOnClickListener {
+            var signIn = Intent(this, ListActivity::class.java)
+            startActivity(signIn)
+        }
+
+    }
+}
